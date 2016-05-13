ab7c410f77cfd82024510410c304066fe09ee709
Lucian Fratila <lucianf@gmail.com>
2016-05-13 12:37:17 +0530

    Initial commit

---
+++ cryptowat.ch.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name Cryptowatch Free trial
+// @namespace https://github.com/lucianf/greasemonkey
+// @include https://cryptowat.ch/*
+// @version 1.0
+// @downloadURL https://raw.githubusercontent.com/lucianf/greasemonkey/master/cryptowat.ch.js
+// ==/UserScript==
+
+$("div#trading-area-overlay").hide();
