749d621adb6500c35f8968d91938f8350c785b89
Lucian Fratila <lucianf@gmail.com>
2016-05-13 12:26:58 +0530

    Remove downloadURL

--- bitcoinwisdom.com.js
+++ bitcoinwisdom.com.js
@@ -4,7 +4,6 @@
 // @include https://bitcoinwisdom.com/*
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @version 0.1
-// @updateURL 
 // @downloadURL https://raw.githubusercontent.com/lucianf/greasemonkey/master/bitcoinwisdom.com.js
 // ==/UserScript==
 
