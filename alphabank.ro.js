a015dae1f6e6c31f67ea1b568e6abcbd0240b307
Lucian Fratila <lucianf@gmail.com>
2016-06-01 10:27:41 +0300

    Initial commit

---
+++ alphabank.ro.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name Alpha Bank defaults
+// @namespace lucianf/greasemonkey
+// @include https://secure*.alphabank.ro/*
+// @version 1.0
+// @downloadURL https://raw.githubusercontent.com/lucianf/greasemonkey/master/alphabank.ro.js
+// ==/UserScript==
+
+var pathname = window.location.pathname;
+
+var prepareScreen = function () {
+    $('.ui-state-default').click();
+    $('input[name="fldOTP"]').focus();
+};
+
+if (pathname == "/corporate/CorpOTPLoginLangRom.jsp") {
+    console.log("setfun");
+    window.setTimeout(prepareScreen, 1000);
+}
