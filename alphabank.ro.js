8b99e269bd56123bf4d11f4847ad30e87a7fe9c4
Lucian Fratila <lucianf@gmail.com>
2016-07-18 10:43:38 +0300

    switch to match

--- alphabank.ro.js
+++ alphabank.ro.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name Alpha Bank defaults
 // @namespace lucianf/greasemonkey
-// @version 1.1
+// @version 1.2
 // @description Dismisses initial popup and focuses OTP field
 // @author Lucian Fratila
-// @match https://secure*.alphabank.ro/*
+// @include https://secure*.alphabank.ro/*
 // @downloadURL https://raw.githubusercontent.com/lucianf/greasemonkey/master/alphabank.ro.js
 // ==/UserScript==
 
@@ -19,6 +19,5 @@ var prepareScreen = function () {
 };
 
 if (pathname == "/corporate/CorpOTPLoginLangRom.jsp") {
-    console.log("setfun");
     window.setTimeout(prepareScreen, 1000);
 }
