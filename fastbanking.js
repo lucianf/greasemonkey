41312f131d0d4a64ffe2aca7acd115679445c281
Lucian Fratila <lucianf@gmail.com>
2017-01-06 11:32:53 +0200

    Fix login & add OTP autosubmit
    Fix jump to token select, add OTP autosubmit on 6 chars

--- fastbanking.js
+++ fastbanking.js
@@ -1,28 +1,69 @@
 // ==UserScript==
 // @name FastBanking defaults
 // @namespace lucianf/greasemonkey
-// @version 1.2
-// @description Fast fwd through initial nav 
+// @version 1.3
+// @description Fast fwd through initial nav
 // @author Lucian Fratila
 // @match https://fastbanking.bancpost.ro/*
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @downloadURL https://raw.githubusercontent.com/lucianf/greasemonkey/master/fastbanking.js
+// @grant GM_setValue
+// @grant GM_getValue
 // ==/UserScript==
 
 var pathname = window.location.pathname;
+//console.log("Path: "+pathname+" ff="+GM_getValue("fbjumpflag"));
 
 if (pathname == "/iBankWeb/login.jsp") {
-  // flag for jumping straight into token auth
-  GM_setValue("fbjumpflag", 1);
-  return;
+    // flag for jumping straight into token auth
+    GM_setValue("fbjumpflag", 1);
+    return;
+}
+
+if (pathname == "/iBankWeb/index.jsp" && GM_getValue("fbjumpflag") == 1) {
+    // choose account
+    $('form[name="choose"]').submit();
+    return;
 }
 
 if (pathname == "/iBankWeb/home/index.do" && GM_getValue("fbjumpflag") == 1) {
-  // coming straight from login, trigger token auth
-  //GM_deleteValue("fbjumpflag");
-  GM_setValue("fbjumpflag", 0);
-  window.location.href = "https://fastbanking.bancpost.ro/iBankWeb/accounts/ListBalanceAccounts.do";
+    // coming straight from login, trigger token auth
+    //GM_deleteValue("fbjumpflag");
+    GM_setValue("fbjumpflag", 0);
+    window.location.href = "https://fastbanking.bancpost.ro/iBankWeb/accounts/ListBalanceAccounts.do";
+    return;
 }
+
+//if (pathname == "/iBankWeb/auth/tokenselect.do" || pathname == "/iBankWeb/auth/verifytokenresp.do") {
+if ($('form[name="response"]').length == 1) {
+    var TwoFactorAuthentication = (function() {
+        var r = {
+                initFormSubmit: function() {
+                    var codeInput = $('.keyboardInput'),
+                        tfauthForm = $('form[name="response"]');
+                    tfauthForm.on('submit', function() {
+                        tfauthForm.find('input[type=submit]').prop('disabled', true);
+                    });
+                    codeInput.on('input', function() {
+                        var text = $(this).val();
+                        if ($.isNumeric(text) && text.length === 6) {
+                            tfauthForm.trigger('submit');
+                            codeInput.off('input');
+                        }
+                    });
+                    codeInput.trigger('focus');
+                }
+            },
+            u = {
+                initialize: function() {
+                    r.initFormSubmit();
+                }
+            };
+        return u;
+    }());
+    $(TwoFactorAuthentication.initialize);
+}
+
 
 if ($('form[name="token"]').length == 1) {
   // select default token and go to auth
